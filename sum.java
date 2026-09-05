e9568ca7d3f44a27aadcf344518768a6f88b0f44
saifullah <saifullahahad0@gmail.com>
2026-09-05 19:21:39 +0530

    find sum of n numbers

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.print(add(10));
+    }
+    public static int add(int n){
+        if (n ==1 ) return 1;
+        return n+ add(n-1);
+   
+
+    }
+}
